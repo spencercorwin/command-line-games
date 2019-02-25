47e5688c4d830395778c755990a1e1bb57ad302b
Spencer Corwin <spencercorwin@icloud.com>
2019-02-24 20:17:27 -0800

    Add config files, edit TTT, create connect 4

---
+++ connect-four.ts
@@ -0,0 +1,132 @@
+import * as prompt from 'prompt';
+
+class ConnectFour {
+  board: object;
+  player: number;
+  constructor() {
+    this.board = {
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+    }
+    this.player = 1;
+  }
+
+  private printBoard(): void {
+    const returnSpot = (input: string | undefined): string => {
+      return input === undefined ? ' ' : input;
+    }
+    console.log(
+        `
+        |${returnSpot(this.board[1][5])}|${returnSpot(this.board[2][5])}|${returnSpot(this.board[3][5])}|${returnSpot(this.board[4][5])}|${returnSpot(this.board[5][5])}|${returnSpot(this.board[6][5])}|${returnSpot(this.board[7][5])}|
+        |${returnSpot(this.board[1][4])}|${returnSpot(this.board[2][4])}|${returnSpot(this.board[3][4])}|${returnSpot(this.board[4][4])}|${returnSpot(this.board[5][4])}|${returnSpot(this.board[6][4])}|${returnSpot(this.board[7][4])}|
+        |${returnSpot(this.board[1][3])}|${returnSpot(this.board[2][3])}|${returnSpot(this.board[3][3])}|${returnSpot(this.board[4][3])}|${returnSpot(this.board[5][3])}|${returnSpot(this.board[6][3])}|${returnSpot(this.board[7][3])}|
+        |${returnSpot(this.board[1][2])}|${returnSpot(this.board[2][2])}|${returnSpot(this.board[3][2])}|${returnSpot(this.board[4][2])}|${returnSpot(this.board[5][2])}|${returnSpot(this.board[6][2])}|${returnSpot(this.board[7][2])}|
+        |${returnSpot(this.board[1][1])}|${returnSpot(this.board[2][1])}|${returnSpot(this.board[3][1])}|${returnSpot(this.board[4][1])}|${returnSpot(this.board[5][1])}|${returnSpot(this.board[6][1])}|${returnSpot(this.board[7][1])}|
+        |${returnSpot(this.board[1][0])}|${returnSpot(this.board[2][0])}|${returnSpot(this.board[3][0])}|${returnSpot(this.board[4][0])}|${returnSpot(this.board[5][0])}|${returnSpot(this.board[6][0])}|${returnSpot(this.board[7][0])}|
+        ---------------
+        |1|2|3|4|5|6|7|
+        `
+    )
+  }
+
+  public playGame(): void {
+    this.printBoard();
+    console.log(`Enter a column player ${this.player}`);
+    prompt.start();
+    prompt.get(['Col'], (err, result) => {
+      if (!this.isValidCol(result.Col)) {
+        console.log('That is not a valid input. Please enter a column number between 1 and 7');
+        this.playGame();
+      } else if (this.isColFull(result.Col)) {
+        console.log('That column is full. Please enter a valid column');
+        this.playGame();
+      } else {
+        this.updateBoard(result.Col);
+        this.checkForVictor();
+      }
+    })
+
+  }
+
+  private checkForVictor(): void {
+    const checkCol = (col: number): string => {
+      for (let i: number = 0; i <= 2; i++) {
+        let initialMoniker: string = this.board[col][i];
+            for (let y: number = 1; y < 4; y++) {
+              if (initialMoniker === undefined) {
+                break;
+              } else if (this.board[col][i + y] !== initialMoniker) {
+                break;
+              } else if (this.board[col][i + y] === initialMoniker && y === 3) {
+                return initialMoniker;
+              }
+            }
+      }
+      return '';
+    }
+
+    const checkRow = (row: number): string => {
+      for (let i: number = 1; i <= 4; i++) {
+        let initialMoniker: string = this.board[i][row];
+            for (let y: number = 0; y < 4; y++) {
+              if (initialMoniker === undefined) {
+                break;
+              } else if (this.board[i + y][row] !== initialMoniker) {
+                break;
+              } else if (this.board[i + y][row] === initialMoniker && y === 3) {
+                return initialMoniker;
+              }
+            }
+      }
+      return '';
+    }
+
+    const declareVictor = (input: string): void => {
+      const victor: string = input === 'O' ? 'Player 1 (O)': 'Player 2 (X)';
+      console.log(victor + ' wins!');
+    }
+
+    for (let col = 1; col <= 7; col++) {
+      if (checkCol(col)) {
+        declareVictor(checkCol(col));
+        return;
+      }
+    }
+
+    for (let row = 0; row <= 5; row++) {
+      if (checkRow(row)) {
+        declareVictor(checkRow(row));
+        return;
+      }
+    }
+    this.player = this.player === 1 ? 2 : 1;
+    this.playGame();
+  }
+
+  private updateBoard(input: string): void {
+    this.board[Number(input.trim())].push(this.returnMoniker());
+  }
+
+  private isValidCol(input: string): boolean {
+    const scrubbedInput = Number(input.trim());
+    return scrubbedInput >= 1 && scrubbedInput <= 7 ? true : false;
+  }
+
+  private returnMoniker(): string {
+    return this.player === 1 ? 'O' : 'X';
+  }
+
+  private isColFull(input: string): boolean {
+    const scrubbedInput = Number(input.trim());
+    return this.board[scrubbedInput].length >= 6 ? true : false;
+  }
+
+}
+
+const game = new ConnectFour();
+game.playGame();
--- tic-tac-toe.ts
+++ tic-tac-toe.ts
@@ -1,110 +1,110 @@
 import * as prompt from 'prompt';
 
 class TicTacToe {
-    board: object;
-    player: number;
-    constructor() {
-        this.board = {
-            1: 1,
-            2: 2,
-            3: 3,
-            4: 4,
-            5: 5,
-            6: 6,
-            7: 7,
-            8: 8,
-            9: 9,
-        };
-        this.player = 1;
-    }
+  board: object;
+  player: number;
+  constructor() {
+      this.board = {
+          1: 1,
+          2: 2,
+          3: 3,
+          4: 4,
+          5: 5,
+          6: 6,
+          7: 7,
+          8: 8,
+          9: 9,
+      };
+      this.player = 1;
+  }
 
-    private printBoard(): void {
-        console.log(
-            `
-            ${this.board[1]} | ${this.board[2]} | ${this.board[3]}\n
-            ----------\n
-            ${this.board[4]} | ${this.board[5]} | ${this.board[6]}\n
-            ----------\n
-            ${this.board[7]} | ${this.board[8]} | ${this.board[9]}\n
-            `
-        )
-    }
+  private printBoard(): void {
+      console.log(
+          `
+          ${this.board[1]} | ${this.board[2]} | ${this.board[3]}
+          ----------
+          ${this.board[4]} | ${this.board[5]} | ${this.board[6]}
+          ----------
+          ${this.board[7]} | ${this.board[8]} | ${this.board[9]}
+          `
+      )
+  }
 
-    public startMove(): void {
-        this.printBoard();
-        console.log(`Player ${this.player} make your move`);
-        prompt.start();
-        prompt.get(['Space'], (err, result) => {
-            if (!this.isAValidSpace(result.Space)) {
-                console.log('That is not valid input. Please enter a number between 1 and 9');
-                this.startMove();
-            } else if (this.isSpaceTaken(result.Space)) {
-                console.log('That space is taken. Try again');
-                this.startMove();
-            } else {
-                console.log('You entered: ' + result.Space.trim());
-                this.updateBoard(result.Space);
-                this.checkForVictory();
-            }
-        });
-    }
+  public startMove(): void {
+      this.printBoard();
+      console.log(`Player ${this.player} make your move`);
+      prompt.start();
+      prompt.get(['Space'], (err, result) => {
+          if (!this.isAValidSpace(result.Space)) {
+              console.log('That is not valid input. Please enter a number between 1 and 9');
+              this.startMove();
+          } else if (this.isSpaceTaken(result.Space)) {
+              console.log('That space is taken. Try again');
+              this.startMove();
+          } else {
+              console.log('You entered: ' + result.Space.trim());
+              this.updateBoard(result.Space);
+              this.checkForVictory();
+          }
+      });
+  }
 
-    private updateBoard(input: string): void {
-        const scrubbedInput = input.trim();
-        const number = Number(scrubbedInput);
-        const symbol = (player: number): string => player === 1 ? 'O' : 'X';
-        this.board[number] = symbol(this.player);
-    }
+  private updateBoard(input: string): void {
+      const scrubbedInput = input.trim();
+      const number = Number(scrubbedInput);
+      const symbol = (player: number): string => player === 1 ? 'O' : 'X';
+      this.board[number] = symbol(this.player);
+  }
 
-    private checkForVictory(): void {
-        const board = this.board;
-        const declareVictor = (victor: number): void => {
-            console.log(board[victor] + ' wins!');
-        };
-        const checkRow = (start: number): boolean => {
-            return board[start] === board[start+1] && board[start] === board[start+2] ? true : false;
-        }
-        const checkCol = (start: number): boolean => {
-            return board[start] === board[start+3] && board[start] === board[start+6] ? true : false;
-        }
-        const checkD1 = (start: number): boolean => {
-            return board[start] === board[start+5] && board[start] === board[start+8] ? true : false;
-        }
-        const checkD2 = (start: number): boolean => {
-            return board[start] === board[start+2] && board[start] === board[start+4] ? true : false;
-        }
-        if (checkRow(1)) {
-            declareVictor(1);
-        } else if (checkRow(4)) {
-            declareVictor(4);
-        } else if (checkRow(7)) {
-            declareVictor(7);
-        } else if (checkCol(1)) {
-            declareVictor(1);
-        } else if (checkCol(2)) {
-            declareVictor(2);
-        } else if (checkCol(3)) {
-            declareVictor(3);
-        } else if (checkD1(1)) {
-            declareVictor(1);
-        } else if (checkD2(3)) {
-            declareVictor(3);
-        } else {
-            this.player = this.player === 1 ? 2 : 1;
-            this.startMove();
-        }
-    }
+  private checkForVictory(): void {
+      const board = this.board;
+      const declareVictor = (victor: number): void => {
+          console.log(board[victor] + ' wins!');
+      };
+      const checkRow = (start: number): boolean => {
+          return board[start] === board[start+1] && board[start] === board[start+2] ? true : false;
+      }
+      const checkCol = (start: number): boolean => {
+          return board[start] === board[start+3] && board[start] === board[start+6] ? true : false;
+      }
+      const checkD1 = (start: number): boolean => {
+          return board[start] === board[start+5] && board[start] === board[start+8] ? true : false;
+      }
+      const checkD2 = (start: number): boolean => {
+          return board[start] === board[start+2] && board[start] === board[start+4] ? true : false;
+      }
+      if (checkRow(1)) {
+          declareVictor(1);
+      } else if (checkRow(4)) {
+          declareVictor(4);
+      } else if (checkRow(7)) {
+          declareVictor(7);
+      } else if (checkCol(1)) {
+          declareVictor(1);
+      } else if (checkCol(2)) {
+          declareVictor(2);
+      } else if (checkCol(3)) {
+          declareVictor(3);
+      } else if (checkD1(1)) {
+          declareVictor(1);
+      } else if (checkD2(3)) {
+          declareVictor(3);
+      } else {
+          this.player = this.player === 1 ? 2 : 1;
+          this.startMove();
+      }
+  }
 
-    private isSpaceTaken(input: string): boolean {
-        const scrubbedInput = Number(input.trim());
-        return typeof this.board[scrubbedInput] === 'number' ? false : true;
-    }
+  private isSpaceTaken(input: string): boolean {
+      const scrubbedInput = Number(input.trim());
+      return typeof this.board[scrubbedInput] === 'number' ? false : true;
+  }
 
-    private isAValidSpace(input: string): boolean {
-        const validSpaces = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        return validSpaces.indexOf(Number(input.trim())) !== -1 ? true : false;
-    }
+  private isAValidSpace(input: string): boolean {
+      const validSpaces = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      return validSpaces.indexOf(Number(input.trim())) !== -1 ? true : false;
+  }
 }
 
-const game = new TicTacToe;
+const game = new TicTacToe();
 game.startMove();
